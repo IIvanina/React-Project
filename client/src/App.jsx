db2dd5932af24915cf76a1a8ae6efb9dee4b5b38
Ivanina <iwanina@gmail.com>
2024-08-06 11:02:59 +0300

    moving logic fromApp.jsx to Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-import {AuthProvider} from "./contexts/authContext.jsx";
+import { AuthProvider } from "./contexts/authContext.jsx";
 import * as authService from "./services/authService.js";
 import Path from "../src/path.js";
 
@@ -21,71 +21,10 @@ import MyBookings from "./components/MyBookings.jsx";
 import Logout from "./components/Logout.jsx";
 
 function App() {
-    const [auth, setAuth] = useState(() => {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('userId');
-
-        return {};
-    });
-    const navigate = useNavigate();
-
-    const loginSubmitHandler = async (values, closeModal) => {
-        try {
-            const result = await authService.login(values.email, values.password);
-
-            setAuth(result);
-            console.log(`Login user Id ${result._id}`);
-            console.log(`Login token ${result.accessToken}`);
-            localStorage.setItem('accessToken', result.accessToken);
-            localStorage.setItem('userId', result._id);
-
-            closeModal();
-
-            navigate(Path.Calendar);
-
-        } catch (error) {
-            console.error("Login failed", error);
-        }
-    };
-
-    const registerSubmitHandler = async (values, closeModal) => {
-        try {
-            const result = await authService.register(values.email, values.password);
-            
-            // Set auth and save tokens in local storage after registration
-            setAuth(result);
-            localStorage.setItem('accessToken', result.accessToken);
-            localStorage.setItem('userId', result._id);
-
-            // Close the registration modal
-            closeModal();
-
-            // Automatically log in the user after registration
-            navigate(Path.Calendar);
-
-        } catch (error) {
-            console.error("Registration failed", error);
-        }
-    };
-
-    const logoutHandler = () => {
-        setAuth({});
-
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('userId');
-    };
-
-    const values = { 
-        loginSubmitHandler,
-        registerSubmitHandler,
-        logoutHandler,
-        email: auth.email,
-        username: auth.username || auth.email,
-        isAuthenticated: !!auth.accessToken,
-    };
+   
 
     return (
-        <AuthProvider value={values}>
+        <AuthProvider >
             <>
                 <Header />
                 <Routes>
--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,13 +1,79 @@
-import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { createContext, useState } from "react";
+import * as authService from "../services/authService.js";
+import Path from "../path.js";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({
     children,
-    value
+    
 }) => {
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('userId');
+
+        return {};
+    });
+    const navigate = useNavigate();
+
+    const loginSubmitHandler = async (values, closeModal) => {
+        try {
+            const result = await authService.login(values.email, values.password);
+
+            setAuth(result);
+            console.log(`Login user Id ${result._id}`);
+            console.log(`Login token ${result.accessToken}`);
+            localStorage.setItem('accessToken', result.accessToken);
+            localStorage.setItem('userId', result._id);
+
+            closeModal();
+
+            navigate(Path.Calendar);
+
+        } catch (error) {
+            console.error("Login failed", error);
+        }
+    };
+
+    const registerSubmitHandler = async (values, closeModal) => {
+        try {
+            const result = await authService.register(values.email, values.password);
+            
+            // Set auth and save tokens in local storage after registration
+            setAuth(result);
+            localStorage.setItem('accessToken', result.accessToken);
+            localStorage.setItem('userId', result._id);
+
+            // Close the registration modal
+            closeModal();
+
+            // Automatically log in the user after registration
+            navigate(Path.Calendar);
+
+        } catch (error) {
+            console.error("Registration failed", error);
+        }
+    };
+
+    const logoutHandler = () => {
+        setAuth({});
+
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('userId');
+    };
+
+    const values = { 
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        email: auth.email,
+        username: auth.username || auth.email,
+        isAuthenticated: !!auth.accessToken,
+    };
+
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     );
